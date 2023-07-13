5d55d6360139b3d93a5f86641c2b66d3b2f981af
whoisnian <zhuchangbao1998@gmail.com>
2023-07-14 02:10:47 +0800

    ADD: android notification from fcm data

--- android/app/src/main/java/com/whoisnian/noti/FcmService.java
+++ android/app/src/main/java/com/whoisnian/noti/FcmService.java
@@ -1,24 +1,50 @@
 package com.whoisnian.noti;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 public class FcmService extends FirebaseMessagingService {
     private static final String TAG = "FcmService";
+    private static final String ChannelID = "messages";
+    private static final CharSequence ChannelName = "FCM messages";
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         Log.d(TAG, "From: " + remoteMessage.getFrom());
         Log.d(TAG, "Message data payload: " + remoteMessage.getData());
-        if (remoteMessage.getNotification() != null) {
-            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
-        }
+
+        createNotificationChannel();
+        Notification notification = new NotificationCompat.Builder(this, ChannelID)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("New Notification")
+                .setContentText(remoteMessage.getData().toString())
+                .setLocalOnly(true)
+                .setAutoCancel(false)
+                .build();
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(0, notification);
     }
 
     @Override
     public void onNewToken(String token) {
         Log.d(TAG, "Refreshed token: " + token);
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(ChannelID, ChannelName, NotificationManager.IMPORTANCE_HIGH);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
