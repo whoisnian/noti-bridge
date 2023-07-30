9a1fd906d8472e1f3c120ad59db93e5315117017
whoisnian <zhuchangbao1998@gmail.com>
2023-07-31 00:07:43 +0800

    ADD: Task.Title for notification

--- server/task/task.go
+++ server/task/task.go
@@ -1,21 +1,23 @@
 package task
 
 const (
-	TypePing = "ping"
-	TypeText = "text"
-	TypeLink = "link"
+	TypePing = "ping" // no content or title
+	TypeText = "text" // text as content, optional title
+	TypeLink = "link" // text or link as content, optional title
 )
 
 type Task struct {
-	Type string
-	Text string
-	Link string
+	Type  string
+	Title string
+	Text  string
+	Link  string
 }
 
 func (t *Task) ToMap() map[string]string {
 	return map[string]string{
-		"Type": t.Type,
-		"Text": t.Text,
-		"Link": t.Link,
+		"Type":  t.Type,
+		"Title": t.Title,
+		"Text":  t.Text,
+		"Link":  t.Link,
 	}
 }
