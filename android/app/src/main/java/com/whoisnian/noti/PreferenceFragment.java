4e3d63b45808e6b11694477fb38d4026ba1650e2
whoisnian <zhuchangbao1998@gmail.com>
2023-11-07 03:17:53 +0800

    ADD: okhttp dep

--- android/app/src/main/java/com/whoisnian/noti/PreferenceFragment.java
+++ android/app/src/main/java/com/whoisnian/noti/PreferenceFragment.java
@@ -1,9 +1,12 @@
 package com.whoisnian.noti;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.preference.EditTextPreference;
 import androidx.preference.ListPreference;
@@ -12,10 +15,23 @@ import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceScreen;
 
+import java.io.IOException;
 import java.util.Collections;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class PreferenceFragment extends PreferenceFragmentCompat {
     private static final String TAG = "PreferenceFragment";
+
+    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    private final OkHttpClient client = new OkHttpClient();
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -35,31 +51,43 @@ public class PreferenceFragment extends PreferenceFragmentCompat {
         namePreference.setSummaryProvider(EditTextPreference.SimpleSummaryProvider.getInstance());
         namePreference.setDialogTitle("Rename");
         namePreference.setDefaultValue(Build.BRAND + " " + Build.MODEL);
+        namePreference.setOnPreferenceChangeListener((p, v) -> {
+            httpPostJson("/api/device", "");
+            return true;
+        });
+        // * Registry
+        EditTextPreference registryPreference = new EditTextPreference(context);
+        registryPreference.setKey("device_registry");
+        registryPreference.setTitle("Registry");
+        registryPreference.setIconSpaceReserved(false);
+        registryPreference.setSummaryProvider(EditTextPreference.SimpleSummaryProvider.getInstance());
+        registryPreference.setDialogTitle("Address");
+        registryPreference.setDefaultValue(getString(R.string.app_registry));
 
         // Group Binding
         PreferenceCategory groupCategory = new PreferenceCategory(context);
         groupCategory.setTitle("Group Binding");
         groupCategory.setIconSpaceReserved(false);
         // * List
-        String[] entries = shared.getStringSet("entries", Collections.emptySet()).toArray(new String[0]);
+        String[] entries = shared.getStringSet("group_entries", Collections.emptySet()).toArray(new String[0]);
         ListPreference listPreference = new ListPreference(context);
-        listPreference.setKey("show_list");
+        listPreference.setKey("group_list");
         listPreference.setTitle("List");
         listPreference.setIconSpaceReserved(false);
-        listPreference.setSummaryProvider(p -> "Currently joined " + listPreference.getEntries().length + " groups");
+        listPreference.setSummaryProvider(p -> "Current " + listPreference.getEntries().length + " groups");
         listPreference.setDialogTitle("Select to quit");
         listPreference.setEntries(entries);
         listPreference.setEntryValues(entries);
-        // * Add New
+        // * Join
         EditTextPreference joinPreference = new EditTextPreference(context);
-        joinPreference.setKey("join_new");
-        joinPreference.setTitle("Add New");
+        joinPreference.setKey("group_join");
+        joinPreference.setTitle("Join");
         joinPreference.setIconSpaceReserved(false);
-        joinPreference.setSummary("Auto create group if not exist");
+        joinPreference.setSummary("Create group if not exist");
         joinPreference.setDialogTitle("Group ID");
         // * Reset
         Preference resetPreference = new Preference(context);
-        resetPreference.setKey("reset");
+        resetPreference.setKey("group_reset");
         resetPreference.setTitle("Reset");
         resetPreference.setIconSpaceReserved(false);
         resetPreference.setSummary("Quit from all groups");
@@ -70,26 +98,57 @@ public class PreferenceFragment extends PreferenceFragmentCompat {
         aboutCategory.setIconSpaceReserved(false);
         // * Version
         Preference versionPreference = new Preference(context);
-        versionPreference.setKey("version");
+        versionPreference.setKey("about_version");
         versionPreference.setTitle("Version");
         versionPreference.setIconSpaceReserved(false);
-        versionPreference.setSummary("1.0");
+        versionPreference.setSummary(BuildConfig.VERSION_NAME);
         // * GitHub
         Preference githubPreference = new Preference(context);
-        githubPreference.setKey("github");
+        githubPreference.setKey("about_github");
         githubPreference.setTitle("GitHub");
         githubPreference.setIconSpaceReserved(false);
-        githubPreference.setSummary("https://github.com/whoisnian/noti-bridge");
+        githubPreference.setSummary(R.string.app_repo);
+        githubPreference.setOnPreferenceClickListener(p -> {
+            Uri uri = Uri.parse(getString(R.string.app_repo));
+            startActivity(new Intent(Intent.ACTION_VIEW, uri));
+            return true;
+        });
 
         screen.addPreference(deviceCategory); // Device Management
         deviceCategory.addPreference(namePreference); // * Name
+        deviceCategory.addPreference(registryPreference); // * Registry
         screen.addPreference(groupCategory); // Group Binding
         groupCategory.addPreference(listPreference); // * List
-        groupCategory.addPreference(joinPreference); // * Add New
+        groupCategory.addPreference(joinPreference); // * Join
         groupCategory.addPreference(resetPreference); // * Reset
         screen.addPreference(aboutCategory); // About
         aboutCategory.addPreference(versionPreference); // * Version
         aboutCategory.addPreference(githubPreference); // * GitHub
         setPreferenceScreen(screen);
+    }
+
+    private void httpPostJson(String path, String jsonBody) {
+        String registry = getPreferenceManager().getSharedPreferences()
+                .getString("device_registry", getString(R.string.app_registry));
+        HttpUrl url = HttpUrl.parse(registry).newBuilder()
+                .encodedPath(path)
+                .build();
+        RequestBody body = RequestBody.create(jsonBody, JSON);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e(TAG, "onFailure: ", e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                response.close();
+                Log.d(TAG, "onResponse: " + response.code());
+            }
+        });
     }
 }
