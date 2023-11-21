32bfab31d6f8dbbbef3149c4e082d3856fb60bd1
whoisnian <zhuchangbao1998@gmail.com>
2023-11-22 01:55:10 +0800

    OPT: android preferences request

--- android/app/src/main/java/com/whoisnian/noti/PreferenceFragment.java
+++ android/app/src/main/java/com/whoisnian/noti/PreferenceFragment.java
@@ -30,13 +30,9 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
-public class PreferenceFragment extends PreferenceFragmentCompat implements Preference.OnPreferenceChangeListener, Preference.OnPreferenceClickListener {
+public class PreferenceFragment extends PreferenceFragmentCompat {
     private static final String TAG = "PreferenceFragment";
-    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
-    private final OkHttpClient client = new OkHttpClient.Builder().connectTimeout(1, TimeUnit.SECONDS).build();
-    private EditTextPreference namePreference, joinPreference;
-    private ListPreference listPreference;
-    private Preference resetPreference, versionPreference, githubPreference;
+    private final OkHttpClient okClient = new OkHttpClient.Builder().connectTimeout(1, TimeUnit.SECONDS).build();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -53,69 +49,32 @@ public class PreferenceFragment extends PreferenceFragmentCompat implements Pref
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preferences, rootKey);
 
-        namePreference = findPreference("device_name");
+        EditTextPreference namePreference = findPreference("device_name");
         namePreference.setDefaultValue(Build.BRAND + " " + Build.MODEL);
-        namePreference.setOnPreferenceChangeListener(this);
-
-        listPreference = findPreference("group_list");
-        String[] entries = getPreferenceManager().getSharedPreferences().getStringSet("group_entries", new HashSet<>()).toArray(new String[0]);
-        listPreference.setEntries(entries);
-        listPreference.setEntryValues(entries);
-        listPreference.setSummaryProvider(p -> "Current " + listPreference.getEntries().length + " groups");
-        listPreference.setOnPreferenceChangeListener(this);
-
-        joinPreference = findPreference("group_join");
-        joinPreference.setOnPreferenceChangeListener(this);
-
-        resetPreference = findPreference("group_reset");
-        resetPreference.setOnPreferenceClickListener(this);
-
-        versionPreference = findPreference("about_version");
-        versionPreference.setSummary(BuildConfig.VERSION_NAME);
-        versionPreference.setOnPreferenceClickListener(this);
-
-        githubPreference = findPreference("about_github");
-        githubPreference.setOnPreferenceClickListener(this);
-    }
-
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference.equals(namePreference)) {
-            String oldValue = namePreference.getText();
+        namePreference.setOnPreferenceChangeListener((p, v) -> {
             JSONObject obj = new JSONObject();
             try {
                 obj.put("Type", 0);
                 obj.put("Token", getPreferenceManager().getSharedPreferences().getString("fcm_token", ""));
-                obj.put("Name", newValue.toString());
+                obj.put("Name", v.toString());
             } catch (JSONException e) {
                 showDialog("Error", e.getMessage());
                 return false;
             }
-            asyncPostJson("/api/device", obj.toString(), new Callback() {
-                @Override
-                public void onFailure(Call call, IOException e) {
-                    runInUiThread(() -> {
-                        namePreference.setText(oldValue);
-                        showDialog("Error", e.getMessage());
-                    });
-                }
+            asyncRequest("/api/device", "POST", obj.toString(), respCallback(res -> namePreference.setText(v.toString())));
+            return false;
+        });
 
-                @Override
-                public void onResponse(Call call, Response response) {
-                    if (!response.isSuccessful()) {
-                        runInUiThread(() -> {
-                            namePreference.setText(oldValue);
-                            showDialog("Error", response.code() + " " + response.message());
-                        });
-                    }
-                    response.close();
-                }
-            });
-        } else if (preference.equals(listPreference)) {
+        String[] entries = getPreferenceManager().getSharedPreferences().getStringSet("group_entries", new HashSet<>()).toArray(new String[0]);
+        ListPreference listPreference = findPreference("group_list");
+        listPreference.setEntries(entries);
+        listPreference.setEntryValues(entries);
+        listPreference.setSummaryProvider(p -> "Current " + listPreference.getEntries().length + " groups");
+        listPreference.setOnPreferenceChangeListener((p, v) -> {
             JSONObject obj = new JSONObject();
             JSONArray arr = new JSONArray();
             try {
-                arr.put(newValue.toString());
+                arr.put(v.toString());
                 obj.put("GIDs", arr);
                 obj.put("Type", 0);
                 obj.put("Token", getPreferenceManager().getSharedPreferences().getString("fcm_token", ""));
@@ -123,37 +82,26 @@ public class PreferenceFragment extends PreferenceFragmentCompat implements Pref
                 showDialog("Error", e.getMessage());
                 return false;
             }
-            asyncDeleteJson("/api/group", obj.toString(), new Callback() {
-                @Override
-                public void onFailure(Call call, IOException e) {
-                    runInUiThread(() -> showDialog("Error", e.getMessage()));
-                }
+            asyncRequest("/api/group", "DELETE", obj.toString(), respCallback(res -> {
+                HashSet<String> entrySet = new HashSet<>(getPreferenceManager().getSharedPreferences().getStringSet("group_entries", new HashSet<>()));
+                entrySet.remove(v.toString());
+                String[] newEntries = entrySet.toArray(new String[0]);
+                listPreference.setEntries(newEntries);
+                listPreference.setEntryValues(newEntries);
+                listPreference.setValue(null);
+                getPreferenceManager().getSharedPreferences().edit()
+                        .putStringSet("group_entries", entrySet)
+                        .commit();
+            }));
+            return false;
+        });
 
-                @Override
-                public void onResponse(Call call, Response response) {
-                    if (response.isSuccessful()) {
-                        runInUiThread(() -> {
-                            HashSet<String> entrySet = new HashSet<>(getPreferenceManager().getSharedPreferences().getStringSet("group_entries", new HashSet<>()));
-                            entrySet.remove(newValue.toString());
-                            String[] entries = entrySet.toArray(new String[0]);
-                            listPreference.setEntries(entries);
-                            listPreference.setEntryValues(entries);
-                            listPreference.setValue(null);
-                            getPreferenceManager().getSharedPreferences().edit()
-                                    .putStringSet("group_entries", entrySet)
-                                    .commit();
-                        });
-                    } else {
-                        runInUiThread(() -> showDialog("Error", response.code() + " " + response.message()));
-                    }
-                    response.close();
-                }
-            });
-        } else if (preference.equals(joinPreference)) {
+        EditTextPreference joinPreference = findPreference("group_join");
+        joinPreference.setOnPreferenceChangeListener((p, v) -> {
             JSONObject obj = new JSONObject();
             JSONArray arr = new JSONArray();
             try {
-                arr.put(newValue.toString());
+                arr.put(v.toString());
                 obj.put("GIDs", arr);
                 obj.put("Type", 0);
                 obj.put("Token", getPreferenceManager().getSharedPreferences().getString("fcm_token", ""));
@@ -162,40 +110,22 @@ public class PreferenceFragment extends PreferenceFragmentCompat implements Pref
                 showDialog("Error", e.getMessage());
                 return false;
             }
-            asyncPutJson("/api/group", obj.toString(), new Callback() {
-                @Override
-                public void onFailure(Call call, IOException e) {
-                    runInUiThread(() -> showDialog("Error", e.getMessage()));
-                }
+            asyncRequest("/api/group", "PUT", obj.toString(), respCallback(res -> {
+                joinPreference.setText("");
+                HashSet<String> entrySet = new HashSet<>(getPreferenceManager().getSharedPreferences().getStringSet("group_entries", new HashSet<>()));
+                entrySet.add(v.toString());
+                String[] newEntries = entrySet.toArray(new String[0]);
+                listPreference.setEntries(newEntries);
+                listPreference.setEntryValues(newEntries);
+                listPreference.setValue(v.toString());
+                getPreferenceManager().getSharedPreferences().edit()
+                        .putStringSet("group_entries", entrySet)
+                        .commit();
+            }));
+            return false;
+        });
 
-                @Override
-                public void onResponse(Call call, Response response) {
-                    if (response.isSuccessful()) {
-                        runInUiThread(() -> {
-                            joinPreference.setText("");
-                            HashSet<String> entrySet = new HashSet<>(getPreferenceManager().getSharedPreferences().getStringSet("group_entries", new HashSet<>()));
-                            entrySet.add(newValue.toString());
-                            String[] entries = entrySet.toArray(new String[0]);
-                            listPreference.setEntries(entries);
-                            listPreference.setEntryValues(entries);
-                            listPreference.setValue(newValue.toString());
-                            getPreferenceManager().getSharedPreferences().edit()
-                                    .putStringSet("group_entries", entrySet)
-                                    .commit();
-                        });
-                    } else {
-                        runInUiThread(() -> showDialog("Error", response.code() + " " + response.message()));
-                    }
-                    response.close();
-                }
-            });
-        }
-        return true;
-    }
-
-    @Override
-    public boolean onPreferenceClick(Preference preference) {
-        if (preference.equals(resetPreference)) {
+        findPreference("group_reset").setOnPreferenceClickListener(p -> {
             JSONObject obj = new JSONObject();
             try {
                 obj.put("GIDs", new JSONArray(getPreferenceManager().getSharedPreferences().getStringSet("group_entries", new HashSet<>())));
@@ -205,47 +135,32 @@ public class PreferenceFragment extends PreferenceFragmentCompat implements Pref
                 showDialog("Error", e.getMessage());
                 return false;
             }
-            asyncDeleteJson("/api/group", obj.toString(), new Callback() {
-                @Override
-                public void onFailure(Call call, IOException e) {
-                    runInUiThread(() -> showDialog("Error", e.getMessage()));
-                }
+            asyncRequest("/api/group", "DELETE", obj.toString(), respCallback(res -> {
+                listPreference.setEntries(new String[0]);
+                listPreference.setEntryValues(new String[0]);
+                listPreference.setValue("");
+                getPreferenceManager().getSharedPreferences().edit()
+                        .putStringSet("group_entries", new HashSet<>())
+                        .commit();
+            }));
+            return true;
+        });
 
-                @Override
-                public void onResponse(Call call, Response response) {
-                    if (response.isSuccessful()) {
-                        runInUiThread(() -> {
-                            listPreference.setEntries(new String[0]);
-                            listPreference.setEntryValues(new String[0]);
-                            listPreference.setValue("");
-                            getPreferenceManager().getSharedPreferences().edit()
-                                    .putStringSet("group_entries", new HashSet<>())
-                                    .commit();
-                        });
-                    } else {
-                        runInUiThread(() -> showDialog("Error", response.code() + " " + response.message()));
-                    }
-                    response.close();
-                }
-            });
-        } else if (preference.equals(versionPreference)) {
-            asyncGet("/status", new Callback() {
-                @Override
-                public void onFailure(Call call, IOException e) {
-                    runInUiThread(() -> showDialog("Error", e.getMessage()));
-                }
+        Preference versionPreference = findPreference("about_version");
+        versionPreference.setSummary(BuildConfig.VERSION_NAME);
+        versionPreference.setOnPreferenceClickListener(p -> {
+            asyncRequest("/status", "GET", null, respCallback(res ->
+                    showDialog("Registry Status", res.code() + " " + res.message())
+            ));
+            return true;
+        });
 
-                @Override
-                public void onResponse(Call call, Response response) {
-                    runInUiThread(() -> showDialog("Registry Status", response.code() + " " + response.message()));
-                    response.close();
-                }
-            });
-        } else if (preference.equals(githubPreference)) {
+        Preference githubPreference = findPreference("about_github");
+        githubPreference.setOnPreferenceClickListener(p -> {
             Uri uri = Uri.parse(getString(R.string.app_repo));
             startActivity(new Intent(Intent.ACTION_VIEW, uri));
-        }
-        return true;
+            return true;
+        });
     }
 
     private void runInUiThread(Runnable action) {
@@ -259,55 +174,40 @@ public class PreferenceFragment extends PreferenceFragmentCompat implements Pref
                 .show();
     }
 
-    private void asyncGet(String path, Callback cb) {
+    private void asyncRequest(String path, String method, String jsonBody, Callback cb) {
         String registry = getPreferenceManager().getSharedPreferences()
                 .getString("device_registry", getString(R.string.app_registry));
         HttpUrl url = HttpUrl.parse(registry).newBuilder()
                 .encodedPath(path)
                 .build();
+        RequestBody body = jsonBody == null ? null : RequestBody.create(jsonBody, MediaType.parse("application/json; charset=utf-8"));
         Request request = new Request.Builder()
                 .url(url)
-                .get()
+                .method(method, body)
                 .build();
-        client.newCall(request).enqueue(cb);
+        okClient.newCall(request).enqueue(cb);
     }
 
-    private void asyncPostJson(String path, String jsonBody, Callback cb) {
-        String registry = getPreferenceManager().getSharedPreferences()
-                .getString("device_registry", getString(R.string.app_registry));
-        HttpUrl url = HttpUrl.parse(registry).newBuilder()
-                .encodedPath(path)
-                .build();
-        Request request = new Request.Builder()
-                .url(url)
-                .post(RequestBody.create(jsonBody, JSON))
-                .build();
-        client.newCall(request).enqueue(cb);
+    private Callback respCallback(RespAction action) {
+        return new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                runInUiThread(() -> showDialog("Error", e.getMessage()));
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) {
+                if (response.isSuccessful()) {
+                    runInUiThread(() -> action.run(response));
+                } else {
+                    runInUiThread(() -> showDialog("Error", response.code() + " " + response.message()));
+                }
+                response.close();
+            }
+        };
     }
 
-    private void asyncPutJson(String path, String jsonBody, Callback cb) {
-        String registry = getPreferenceManager().getSharedPreferences()
-                .getString("device_registry", getString(R.string.app_registry));
-        HttpUrl url = HttpUrl.parse(registry).newBuilder()
-                .encodedPath(path)
-                .build();
-        Request request = new Request.Builder()
-                .url(url)
-                .put(RequestBody.create(jsonBody, JSON))
-                .build();
-        client.newCall(request).enqueue(cb);
-    }
-
-    private void asyncDeleteJson(String path, String jsonBody, Callback cb) {
-        String registry = getPreferenceManager().getSharedPreferences()
-                .getString("device_registry", getString(R.string.app_registry));
-        HttpUrl url = HttpUrl.parse(registry).newBuilder()
-                .encodedPath(path)
-                .build();
-        Request request = new Request.Builder()
-                .url(url)
-                .delete(RequestBody.create(jsonBody, JSON))
-                .build();
-        client.newCall(request).enqueue(cb);
+    private interface RespAction {
+        void run(Response response);
     }
 }
